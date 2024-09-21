4219089d9bad1f34d9d5f912dfe8b9c2122d3fcf
it174963 <sdomenstud@gmail.com>
2024-09-21 16:29:24 +0300

    update 4.0

--- app/app.py
+++ app/app.py
@@ -1,9 +1,25 @@
-from flask import Flask
+from flask import Flask,render_template, request, redirect,url_for
+
 app = Flask(__name__)
+
+todos = []
 
 @app.route('/')
 def home():
-	return "Simple CI/CD Pipeline project!"
+ return render_template('index.html' , todos=todos)
+
+@app.route('/add', methods=['POST'])
+def add():
+ task = request.form.get('task')
+ if task:
+  todos.append(task)
+ return redirect(url_for('home'))
+
+@app.route('/delete/<int:task_id>')
+def delete(task_id):
+ if 0<= task_id < len(todos):
+  todos.pop(task_id)
+ return	redirect(url_for('home'))
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=5000)
