83b4c01cb78079bda84713b3cd637ca8039f3e5c
it174963 <sdomenstud@gmail.com>
2024-09-21 17:11:31 +0300

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,24 +1,26 @@
-from flask import Flask,render_template, request, redirect,url_for
+from flask import Flask,render_template, request, redirect, url_for
+import os
 
 app = Flask(__name__)
 
-todos = []
+todo = []
 
 @app.route('/')
 def home():
- return render_template('index.html',todos=todos)
+ print("Templates dir contents:", os.listdir('templates'))
+ return render_template('index.html',todo=todo)
 
 @app.route('/add',methods=['POST'])
 def add():
  task = request.form.get('task')
  if task:
-  todos.append(task)
+  todo.append(task)
  return redirect(url_for('home'))
 
 @app.route('/delete/<int:task_id>')
 def delete(task_id):
- if 0<= task_id < len(todos):
-  todos.pop(task_id)
+ if 0<= task_id < len(todo):
+  todo.pop(task_id)
  return	redirect(url_for('home'))
 
 if __name__ == '__main__':
